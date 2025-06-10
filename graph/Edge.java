c765afcd4ee82f55b69a862e6bcc0665078d9da9
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-10 15:41:03 -0500

    constructores

--- graph/Edge.java
+++ graph/Edge.java
@@ -3,5 +3,15 @@ package graph;
 public class Edge<E> {
     private Vertex<E> refDest;
     private int weigth;
+
+    public Edge(Vertex<E> refDest){
+        this(refDest,-1);
+    }
+
+    public Edge(Vertex<E> refDest, int weigth){
+        this.refDest = refDest;
+        this.weigth = weigth;
+    }
+
     
 }
