f6b8da9af97aa9638940d361d8119e2af85246f3
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-10 15:34:58 -0500

    getter de data metodo equals

--- graph/Vertex.java
+++ graph/Vertex.java
@@ -1,8 +1,27 @@
 package graph;
 
-import java.util.ListLinked;
-
 public class Vertex<E> {
     private E data;
     protected ListLinked<Edge<E>> listAdj;
+
+    public Vertex(E data){
+        this.data = data;
+        listAdj = new ListLinked<Edge<E>>();
+    }
+
+    public E getData(){
+        return data;
+    }
+
+    public boolean equals(Object o){
+        if (o instanceof Vertex<?>) {
+            Vertex<E> v = (Vertex<E>)o;
+            return this.data.equals(v.data);
+        }
+        return false;
+    }
+
+    public String toString(){
+        
+    }
 }
