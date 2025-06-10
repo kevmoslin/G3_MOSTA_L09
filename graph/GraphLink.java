4590ccdbe958d7f86a2ebdf02f6f949b0b11eb15
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-10 16:19:08 -0500

    metodos Search

--- graph/GraphLink.java
+++ graph/GraphLink.java
@@ -17,6 +17,45 @@ public class GraphLink<E> {
 
     }
 
+    public boolean searchVertex(E data){
+        for (Vertex<E> v : listVertex){
+            if (v.getData().equals(data)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchEdge(E v, E z){
+        Vertex<E> v1 = null;
+        Vertex<E> v2 = null;
+
+        for (Vertex<E> vertex : listVertex) {
+            if (vertex.getData().equals(v)){
+                v1 = vertex;
+            }
+            if (vertex.getData().equals(z)) {
+                v2 = vertex;
+            }
+        }
+
+        if (v1 == null || v2 == null) return false;
+
+        return v1.listAdj.contains(new Edge<E>(v2));
+    }
+
+    public void removeVertex(E data){
+
+    }
+
+    public void removeEdge(E v, E z){
+
+    }
+
+    public void dfs(E start){
+
+    }
+
     public String toString(){
         return this.listVertex.toString();
     }
