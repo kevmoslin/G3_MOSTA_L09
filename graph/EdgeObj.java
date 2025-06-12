97d0999e7cb4e4b1c775ced394ce160b8646c9a0
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 14:44:31 -0600

    Constructor

--- graph/EdgeObj.java
+++ graph/EdgeObj.java
@@ -6,5 +6,10 @@ public class EdgeObj<V, E> {
     protected VertexObj<V, E> endVertex2;
     protected int position;
 
-    
+    public EdgeObj(VertexObj<V, E> vert1, VertexObj<V, E> vert2, E info, int position) {
+        this.endVertex1 = vert1;
+        this.endVertex2 = vert2;
+        this.info = info;
+        this.position = position;
+    }
 }
