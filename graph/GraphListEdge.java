f33d56cf5d3c0c8c088ac9b8e83ad66a19e625be
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 16:03:51 -0600

    metodos q pide realizar

--- graph/GraphListEdge.java
+++ graph/GraphListEdge.java
@@ -10,4 +10,26 @@ public class GraphListEdge<V, E> {
         this.secVertex = new ArrayList<>();
         this.secEdge = new ArrayList<>();
     }
+
+    public void insertVertex(V info){
+
+    }
+
+    public void insertEdge(V v1, V v2){
+
+    }
+
+    public boolean searchVertex(V info){
+
+    }
+
+    public boolean searchEdge(V v1, V v2){
+
+    }
+
+    public void bfs(V startInfo){
+
+    }
+
+    
 }
