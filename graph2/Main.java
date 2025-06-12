b1f5620e457a853b06a2353342bb17122f202f00
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 16:35:58 -0600

    insertamos vertices y aristas

--- graph2/Main.java
+++ graph2/Main.java
@@ -1,9 +1,16 @@
 package graph2;
 
-import graph.*;
-
 public class Main {
     public static void main(String[] args) {
         GraphListEdge<String, Integer> grafo = new GraphListEdge<>();
+
+        grafo.insertVertex("A");
+        grafo.insertVertex("B");
+        grafo.insertVertex("C");
+        grafo.insertVertex("D");
+
+        grafo.insertEdge("A", "B");
+        grafo.insertEdge("A", "C");
+        grafo.insertEdge("B", "D");
     }
 }
