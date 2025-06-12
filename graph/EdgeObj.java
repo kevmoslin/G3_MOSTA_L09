c4f6396282118f927d8f8de0559896fa13344112
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 14:44:18 -0600

    atributos de Edgeobj

--- graph/EdgeObj.java
+++ graph/EdgeObj.java
@@ -1,5 +1,10 @@
 package graph;
 
 public class EdgeObj<V, E> {
+    protected E info;
+    protected VertexObj<V, E> endVertex1;
+    protected VertexObj<V, E> endVertex2;
+    protected int position;
+
     
 }
