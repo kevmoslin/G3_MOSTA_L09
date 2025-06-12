f92165f1ec700c15245dba4106cc2336c24e6ff9
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 17:40:39 -0600

    insertando aristas y vertices

--- Ejercicio5/Main.java
+++ Ejercicio5/Main.java
@@ -3,5 +3,11 @@ package Ejercicio5;
 public class Main {
     public static void main(String[] args) {
         GraphLink<String> grafo = new GraphLink<>();
+
+        grafo.insertVertex("A");
+        grafo.insertVertex("B");
+        grafo.insertVertex("C");
+        grafo.insertEdge("A", "B");
+        grafo.insertEdge("B", "C");
     }
 }
