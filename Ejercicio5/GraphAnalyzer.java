c5a70af1934099b11429b507d398d8f1ee45fdf9
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 17:32:04 -0600

    para ver el camino

--- Ejercicio5/GraphAnalyzer.java
+++ Ejercicio5/GraphAnalyzer.java
@@ -15,4 +15,29 @@ public class GraphAnalyzer<E> {
         }
         return -1;
     }
+
+    public boolean camino(){
+        int contadorGrado = 0;
+        for (Vertex<E> v : graph.listVertex){
+            int grado = v.listAdj.size();
+            if (grado == 1) {
+                contadorGrado++;
+            } else if (grado != 2) {
+                return false;
+            }
+        }
+        return contadorGrado == 2;
+    }
+
+    public boolean ciclo(){
+
+    }
+
+    public boolean rueda(){
+
+    }
+
+    public boolean completo(){
+
+    }
 }
