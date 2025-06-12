b11dba246994988cb9a077dfed14acc76efee4c9
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 14:41:39 -0600

    constructor

--- graph/VertexObj.java
+++ graph/VertexObj.java
@@ -3,4 +3,11 @@ package graph;
 public class VertexObj<V, E> {
     protected V info;
     protected int position;
+
+    public VertexObj(V info, int position) {
+        this.info = info;
+        this.position = position;
+    }
+
+    
 }
