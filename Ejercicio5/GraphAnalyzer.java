05777a4c266e8a43eff92dfe366ebccb6b21c07a
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 17:20:37 -0600

    para obterner el grado

--- Ejercicio5/GraphAnalyzer.java
+++ Ejercicio5/GraphAnalyzer.java
@@ -6,4 +6,13 @@ public class GraphAnalyzer<E> {
     public GraphAnalyzer(GraphLink<E> graph) {
         this.graph = graph;
     }
+
+    public int grado(E vertexData){
+        for (Vertex<E> v : graph.listVertex){
+            if (v.getData().equals(vertexData)) {
+                return v.listAdj.size();
+            }
+        }
+        return -1;
+    }
 }
