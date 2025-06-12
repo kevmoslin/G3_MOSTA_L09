4e97ac096bdf0a1bbc910862718d3a09c661cc44
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 16:12:18 -0600

    metodos de busca

--- graph/GraphListEdge.java
+++ graph/GraphListEdge.java
@@ -20,11 +20,24 @@ public class GraphListEdge<V, E> {
     }
 
     public boolean searchVertex(V info){
-
+        return getVertex(info) != null;
     }
 
     public boolean searchEdge(V v1, V v2){
+        VertexObj<V, E> vert1 = getVertex(v1);
+        VertexObj<V, E> vert2 = getVertex(v2);
 
+        if (vert1 == null || vert2 == null) {
+            return false;
+        }
+
+        for (EdgeObj<V, E> edge : secEdge){
+            if ((edge.endVertex1.equals(vert1) && edge.endVertex2.equals(vert2)) || (edge.endVertex1.equals(vert2)) && (edge.endVertex2.equals(vert1))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void bfs(V startInfo){
