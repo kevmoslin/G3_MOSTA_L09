71d581e637aeaf1941bafb63e06ca71dca8998d0
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 18:49:49 -0600

    atributo y constructor

---
+++ ejercicio9/GraphAdvancedAnalyzer.java
@@ -0,0 +1,9 @@
+package ejercicio9;
+
+public class GraphAdvancedAnalyzer<E> {
+    private GraphLink<E> graph;
+
+    public GraphAdvancedAnalyzer(GraphLink<E> graph) {
+        this.graph = graph;
+    }
+}
