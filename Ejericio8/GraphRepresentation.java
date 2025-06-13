d512bd24b6a15e9c2d6598f3f4d0f23e6ed4b9f5
kevmoslin <kevinmostacerolinares@gmail.com>
2025-06-12 18:37:00 -0600

    metodo 1

--- Ejericio8/GraphRepresentation.java
+++ Ejericio8/GraphRepresentation.java
@@ -3,5 +3,22 @@ package Ejericio8;
 public class GraphRepresentation<E> {
     private GraphLink<E> graph;
 
-    
+    public GraphRepresentation(GraphLink<E> graph) {
+        this.graph = graph;
+    }
+
+    public String formalRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Vertices:\n");
+        for (Vertex<E> v : graph.listVertex) {
+            sb.append(v.getData()).append("\n");
+        }
+        sb.append("Aristas:\n");
+        for (Vertex<E> v : graph.listVertex) {
+            for (Edge<E> edge : v.listAdj) {
+                sb.append(v.getData()).append(" -> ").append(edge.getRefDest().getData()).append("\n");
+            }
+        }
+        return sb.toString();
+    }
 }
